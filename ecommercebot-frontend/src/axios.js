a8355245a3143c46ee27a0bd21c2f942eff99ca7
Deep Manek <dpmanek@gmail.com>
2021-03-26 18:25:32 +0530

    pushing updates

--- ecommercebot-frontend/src/axios.js
+++ ecommercebot-frontend/src/axios.js
@@ -1,6 +1,6 @@
 // Creating base url
 
-const instance = "https://508878bfc12c.ngrok.io/";
+const instance = "https://50272d634c4d.ngrok.io/";
 //const instance = "http://localhost:3000/";
 
 export default instance;
