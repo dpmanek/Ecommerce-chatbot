8dc0bee0a3ce70d5403caa1fbae8638b6291d335
Deep Manek <dpmanek@gmail.com>
2021-04-07 12:16:46 +0530

    conflict check

--- ecommmercebot/git changes.js
+++ ecommmercebot/git changes.js
@@ -1,9 +1,9 @@
 /* git branch <branchname>
 git checkout <branchname> 
 git checkout -b <branchname>
-git checkout -D <branchname>
+git branch -D <branchname>
 git push origin --delete <branchname>
 git merge <branchname>
  */
 
-//new comment in branch deep
+//conflict comment
